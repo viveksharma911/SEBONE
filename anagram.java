a552a1d6b92c4e72cc9067cd0daf7b4760356d61
vivek9111 <vsharma@sebonetechnologies.com>
2022-03-13 12:18:22 -0700

    update

--- anagram.java
+++ anagram.java
@@ -4,26 +4,37 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class anagram {
-	
+	            
+	    static boolean bool(String s1, String s2) {
+	    	boolean flag = false;
+	    	if(s2.length() == s1.length()) {
+                char[]arr = s2.toCharArray();
+                Arrays.sort(arr);
+                
+                char[]arr2 = s1.toCharArray();
+                Arrays.sort(arr2);
+                
+               flag = Arrays.equals(arr, arr2);
+               
+                }
+	    	
+	    	return flag;
+	    }
                 public static void main(String[] arg) {
                 	
-                	System.out.println("hello");
                 	
-                	System.out.println("sajal");
-                	Scanner sc= new Scanner(System.in); 
+                	
+                
+                	Scanner sc = new Scanner(System.in);
                     String str = sc.nextLine();
                     String str1 = sc.nextLine();
+                     
+                    boolean flag = bool(str,str1);
                     
-                    if(str.length() == str1.length()) {
-                    char[]arr = str.toCharArray();
-                    Arrays.sort(arr);
-                    
-                    char[]arr2 = str1.toCharArray();
-                    Arrays.sort(arr2);
-                    
-                    boolean flag = Arrays.equals(arr, arr2);
-                    System.out.println(flag);
-                    
+                    if(flag == true) {
+                    	System.out.println("This is a anagram");
+                    }else {
+                    	System.out.println("This is not anagram");
                     }
 
                 	
